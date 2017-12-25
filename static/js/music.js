06ceff63b516039c22a18ecb3fe315f2b79fd649
MaiCong <i@maicong.me>
2017-12-25 15:16:30 +0800

    搜索结果增加 载入更多 功能

--- static/js/music.js
+++ static/js/music.js
@@ -4,7 +4,7 @@
  *
  * @author  MaiCong <i@maicong.me>
  * @link    https://github.com/maicong/music
- * @since   1.5.1
+ * @since   1.5.3
  *
  */
 
@@ -77,102 +77,142 @@ $(function() {
     submit: function submit(v) {
       v.preventDefault();
       if (this.isFormValid()) {
+        var input = $.trim($('#j-input').val());
         var filter = $('#j-input').data('filter');
-        var postData = {
-          music_input: $.trim($('#j-input').val()),
-          music_filter: filter,
-          music_type: $('input[name="music_type"]:checked').val()
-        };
+        var type = filter === 'url' ? '_' : $('input[name="music_type"]:checked').val();
+        var page = 1;
+        var $more = $('<div class="aplayer-more">载入更多</div>');
+        var isload = false;
+        var ajax = function (input, filter, type, page) {
+          $.ajax({
+            type: 'POST',
+            url: location.href.split('?')[0],
+            timeout: 30000,
+            data: {
+              input: input,
+              filter: filter,
+              type: type,
+              page: page
+            },
+            dataType: 'json',
+            beforeSend: function () {
+              isload = true;
+              if (page === 1) {
+                $('#j-input').attr('disabled', true);
+                $('#j-submit').button('loading');
+              } else {
+                $more.text('请稍后...');
+              }
+            },
+            success: function (result) {
+              if (result.code === 200 && result.data) {
+                result.data.map(function(v) {
+                  if (!v.title) v.title = '暂无';
+                  if (!v.author) v.author = '暂无';
+                  if (!v.pic) v.pic = nopic;
+                  if (!v.lrc) v.lrc = '[00:00.00] 暂无歌词';
+                  if (!/\[00:(\d{2})\./.test(v.lrc)) {
+                    v.lrc = '[00:00.00] 无效歌词';
+                  }
+                });
+                var setValue = function setValue(data) {
+                  $('#j-link').val(data.link);
+                  $('#j-link-btn').attr('href', data.link);
+                  $('#j-src').val(data.url);
+                  $('#j-src-btn').attr('href', data.url);
+                  $('#j-name').val(data.title);
+                  $('#j-author').val(data.author);
+                };
 
-        if (filter === 'url') {
-          postData.music_type = '_';
+                if (page === 1) {
+                  if (player) {
+                    player.pause();
+                  }
+                  setValue(result.data[0]);
+                  $('#j-validator').slideUp();
+                  $('#j-main').slideDown();
+
+                  player = new APlayer({
+                    element: $('#j-player')[0],
+                    autoplay: false,
+                    narrow: false,
+                    showlrc: 1,
+                    mutex: false,
+                    mode: 'circulation',
+                    preload: 'metadata',
+                    theme: '#0e90d2',
+                    music: result.data
+                  });
+
+                  $('#j-player').append($more);
+
+                  $more.on('click', function() {
+                    if (isload) return;
+                    page++;
+                    ajax(input, filter, type, page);
+                  });
+                } else {
+                  player.addMusic(result.data);
+                }
+
+                player.on('canplay', function() {
+                  player.play();
+                });
+                player.on('play', function() {
+                  var data = result.data[player.playIndex];
+                  var img = new Image();
+                  img.src = data.pic;
+                  img.onerror = function() {
+                    $('.aplayer-pic').css('background-image', 'url(' + nopic + ')');
+                  };
+                  setValue(data);
+                });
+                if (result.data.length < 10) {
+                  $more.hide();
+                } else {
+                  $more.text('载入更多');
+                }
+              } else {
+                if (page === 1) {
+                  $('#j-input')
+                    .closest('.am-form-group')
+                    .find('.am-alert')
+                    .html(result.error || '(°ー°〃) 服务器好像罢工了')
+                    .show();
+                } else {
+                  $more.text('没有了');
+                  setTimeout(function() {
+                    $more.slideUp();
+                  }, 1000);
+                }
+              }
+            },
+            error: function (e, t) {
+              if (page === 1) {
+                var err = '(°ー°〃) 出了点小问题，请重试';
+                if (t === 'timeout') {
+                  err = '(°ー°〃) 请求超时了，可能是您的网络慢';
+                }
+                $('#j-input')
+                  .closest('.am-form-group')
+                  .find('.am-alert')
+                  .html(err)
+                  .show();
+              } else {
+                $more.text('(°ー°〃) 加载失败了，点击重试');
+              }
+            },
+            complete: function () {
+              isload = false;
+              if (page === 1) {
+                $('#j-input').attr('disabled', false);
+                $('#j-submit').button('reset');
+              }
+            }
+          });
         }
 
-        return $.ajax({
-          type: 'POST',
-          url: location.href.split('?')[0],
-          timeout: 30000,
-          data: postData,
-          dataType: 'json',
-          beforeSend: function beforeSend() {
-            $('#j-input').attr('disabled', true);
-            $('#j-submit').button('loading');
-          },
-          success: function success(result) {
-            if (result.code === 200 && result.data) {
-              result.data.map(function(v) {
-                if (!v.title) v.title = '暂无';
-                if (!v.author) v.author = '暂无';
-                if (!v.pic) v.pic = nopic;
-                if (!v.lrc) v.lrc = '[00:00.00] 暂无歌词';
-                if (!/\[00:(\d{2})\./.test(v.lrc)) {
-                  v.lrc = '[00:00.00] 无效歌词';
-                }
-              });
-              if (player) {
-                player.pause();
-              }
-              var setValue = function setValue(data) {
-                $('#j-link').val(data.link);
-                $('#j-link-btn').attr('href', data.link);
-                $('#j-src').val(data.url);
-                $('#j-src-btn').attr('href', data.url);
-                $('#j-name').val(data.title);
-                $('#j-author').val(data.author);
-              };
-
-              setValue(result.data[0]);
-
-              $('#j-validator').slideUp();
-              $('#j-main').slideDown();
-
-              player = new APlayer({
-                element: $('#j-player')[0],
-                autoplay: false,
-                narrow: false,
-                showlrc: 1,
-                mutex: false,
-                mode: 'circulation',
-                preload: 'metadata',
-                theme: '#0e90d2',
-                music: result.data
-              });
-              player.on('canplay', function() {
-                player.play();
-              });
-              player.on('play', function() {
-                var data = result.data[player.playIndex];
-                var img = new Image();
-                img.src = data.pic;
-                img.onerror = function() {
-                  $('.aplayer-pic').css('background-image', 'url(' + nopic + ')');
-                };
-                setValue(data);
-              });
-            } else {
-              $('#j-input')
-                .closest('.am-form-group')
-                .find('.am-alert')
-                .html(result.error || '(°ー°〃) 服务器好像罢工了')
-                .show();
-            }
-          },
-          error: function error(e, t) {
-            var err = '(°ー°〃) 出了点小问题，请重试';
-            if (t === 'timeout') {
-              err = '(°ー°〃) 请求超时了，可能是您的网络慢';
-            }
-            $('#j-input')
-              .closest('.am-form-group')
-              .find('.am-alert')
-              .html(err)
-              .show();
-          },
-          complete: function complete() {
-            $('#j-input').attr('disabled', false);
-            $('#j-submit').button('reset');
-          }
-        });
+        ajax(input, filter, type, page);
       }
     }
   });
