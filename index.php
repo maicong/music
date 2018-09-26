672b660a79bc5960b8036acc4e91064c0dbbd2aa
MaiCong <i@maicong.me>
2018-09-26 11:14:13 +0800

    更新版本号

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * @author  MaiCong <i@maicong.me>
  * @link    https://github.com/maicong/music
- * @since   1.6.1
+ * @since   1.6.2
  *
  */
 
@@ -13,7 +13,7 @@
 define('MC_CORE', true);
 
 // 定义版本
-define('MC_VERSION', '1.6.1');
+define('MC_VERSION', '1.6.2');
 
 // 核心文件目录
 define('MC_CORE_DIR', __DIR__ . '/core');
